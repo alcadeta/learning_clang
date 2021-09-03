a92a7303c47d89dd7fdac84219d610019e297b8e
Alican Demirtas <demirtasdev@hotmail.com>
2021-09-03 22:51:10 +0100

    C Basics Part 1

---
+++ cmake-build-debug/CMakeFiles/clang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clang.dir/main.c.o"
+  "clang"
+  "clang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <strings.h>
+
+// "Fonkisyon Tanit" veya "Declare a Function"
+bool ondan_yuksektir(int sayi) {
+    if (sayi > 10) return true;
+    else return false;
+}
+
+int main() {
+    // string degiskeni tanit
+    char yaziDegiskeni[21] = "bu bir yazi degiskeni";
+
+    // string degiskenini konsola yaz
+    printf("%s\n", yaziDegiskeni);
+
+    // sayi degiskeni tanit
+    int sayi = 10;
+
+    // sayi degiskenini konsola yaz
+    printf("%i\n", sayi);
+
+    // sayi'nin ondan yuksek oldugunu belirle
+    bool cevap = ondan_yuksektir(sayi);
+    if (cevap == true) {
+        printf("SAYI 10'DAN YUKSEK.");
+    }
+    else {
+        printf("SAYI 10 VEYA 10'DAN KUCUK");
+    }
+
+    // FOR DONGUSU veya FOR LOOP
+    // baslangic degeri belirle      =>      int i = 0
+    // kondisyon veya "condition"    =>      i < sizeof(yaziDegiskeni)
+    // operasyon veya "operator      =>      i++
+    for (int i = 0; i < sizeof(yaziDegiskeni); i++) {
+        printf("%c\n", yaziDegiskeni[i]);
+    }
+
+    // STRING INTERPOLATION
+    // "\n"    =>     bi alt satira gec     veya    ENTER
+    // "%c"    =>     karakter degiskeni    veya    char variable
+    // "%s"    =>     yazi degiskeni        veya    string variable
+    // "%i"    =>     sayi degiskeni        veya    int variable
+    // "%lu"   =>                           veya    unassigned long
+
+    printf("\n\n");
+    char benim_yazim[10] = "BABANOVICH";
+    printf("BENIM YAZIM: %s\nBENIM SAYIM %lu", benim_yazim, sizeof(benim_yazim));
+
+    // CEVAP:
+    // BENIM YAZIM: BABANOVICH
+    // BENIM SAYIM: 10
+
+    return 0;
+}
+
